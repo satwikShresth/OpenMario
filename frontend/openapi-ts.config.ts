38094b4ca37f5bd2a4b9135f6031165468dc0dc2
satwikShresth <satwikshresth@gmail.com>
2025-01-20 12:53:08 -0500

    changing the porj structure for frontend

--- frontend/openapi-ts.config.ts
+++ frontend/openapi-ts.config.ts
@@ -1,26 +1,40 @@
-import { defineConfig } from "@hey-api/openapi-ts"
+import { defineConfig, defaultPlugins } from "@hey-api/openapi-ts"
 
 export default defineConfig({
-  client: "legacy/axios",
+  client: "@hey-api/client-axios",
   input: "../openapi.json",
-  output: "./app/client",
+  output: ".client",
   plugins: [
+    ...defaultPlugins,
     {
       name: "@hey-api/sdk",
       asClass: true,
+      validator: "zod",
       operationId: true,
-      methodNameBuilder: (operation) => {
-        // @ts-ignore
-        let name: string = operation.name
-        // @ts-ignore
-        let service: string = operation.service
-
-        if (service && name.toLowerCase().startsWith(service.toLowerCase())) {
-          name = name.slice(service.length)
-        }
-
-        return name.charAt(0).toLowerCase() + name.slice(1)
-      },
+      //methodNameBuilder: (operation) => {
+      // @ts-ignore
+      //let name: string = operation.name
+      // @ts-ignore
+      /*
+{
+id: 'getBooks',
+method: 'get',
+path: '/books',
+description: 'Get all books with optional filtering',
+tags: [ 'Books' ],
+parameters: { query: { title: [Object], genre: [Object], pub_year: [Object] } },
+responses: { '200': { mediaType: 'application/json', schema: [Object] } }
+}
+       * */
+      //  let service: string = operation.service
+      //  console.log(operation)
+      //  if (service && name.toLowerCase().startsWith(service.toLowerCase())) {
+      //    name = name.slice(service.length)
+      //  }
+      //  console.log(name)
+      //
+      //  return name[0].toLowerCase() + name.slice(1)
+      //},
     },
   ],
 })
