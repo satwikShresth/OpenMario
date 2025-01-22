10144dca28d101c8101f24a7b4b146bf424b9ee7
satwikShresth <satwikshresth@gmail.com>
2025-01-21 19:03:36 -0500

    fixing styling because why not

--- front/app/nav.tsx
+++ front/app/nav.tsx
@@ -1,17 +1,26 @@
 import { NavLink } from 'react-router';
 
-
 export default () => {
   const navLinkClasses = ({ isActive }: {
     isActive: boolean;
   }): string => {
-    return `px-4 py-2 ${isActive ? 'text-blue-600 font-medium' : 'text-white hover:text-gray-900'}`;
+    return `
+      px-6 py-4
+      font-medium
+      transition-colors
+      duration-200
+      border-b-2
+      ${isActive
+        ? 'text-blue-400 border-blue-400'
+        : 'text-gray-400 border-transparent hover:text-gray-200 hover:border-gray-400'
+      }
+    `;
   };
 
   return (
-    <nav className="bg-gray-700 border-b">
-      <div className="px-4">
-        <div className="flex space-x-4 h-14 items-center">
+    <nav className="bg-black border-b border-gray-800">
+      <div className="mx-auto px-4">
+        <div className="flex h-16 items-center">
           <NavLink to="/" end className={navLinkClasses}>
             Home
           </NavLink>
