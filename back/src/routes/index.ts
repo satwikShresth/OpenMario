19573e6814be96eeda2c27afd3d44b09a832b9ad
satwikShresth <satwikshresth@gmail.com>
2025-02-10 00:53:47 -0500

    adding a indivisual rate limiter

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -4,17 +4,25 @@ import authRoutes from './auth.routes.ts';
 import authorsRoutes from './authors.routes.ts';
 import rateLimit from 'express-rate-limit';
 
-const authLimiter = rateLimit({
-   windowMs: 60 * 60 * 1000, // 1 hour
-   max: 5, // Limit each IP to 5 requests per windowMs
-   message: 'Too many authentication attempts, please try again later',
+const apiLimiter = rateLimit({
+   windowMs: 2 * 60 * 1000,
+   max: 50,
+   message: 'Too many API requests from this IP, please try again later',
    standardHeaders: true,
    legacyHeaders: false,
 });
 
-const apiLimiter = rateLimit({
-   windowMs: 15 * 60 * 1000, // 15 minutes
-   max: 50, // Limit each IP to 50 requests per windowMs
+const genLimiter = rateLimit({
+   windowMs: 2 * 60 * 1000,
+   max: 100,
+   message: 'Too many API requests from this IP, please try again later',
+   standardHeaders: true,
+   legacyHeaders: false,
+});
+
+const genLimiter2 = rateLimit({
+   windowMs: 2 * 60 * 1000,
+   max: 100,
    message: 'Too many API requests from this IP, please try again later',
    standardHeaders: true,
    legacyHeaders: false,
@@ -23,9 +31,9 @@ const apiLimiter = rateLimit({
 export default () => {
    const router = Router();
 
-   router.use('/', authLimiter, authRoutes());
-   router.use('/books', apiLimiter, booksRoutes());
-   router.use('/authors', apiLimiter, authorsRoutes());
+   router.use('/', apiLimiter, authRoutes());
+   router.use('/books', genLimiter, booksRoutes());
+   router.use('/authors', genLimiter2, authorsRoutes());
 
    return router;
 };
