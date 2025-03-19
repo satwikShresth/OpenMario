e3ce4c47ad98f4df5684843565d2fea15456bb18
satwikShresth <satwikshresth@gmail.com>
2025-03-19 13:00:26 -0400

    changes for dev

--- back/tests/index.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const port = 3000;
-const host = 'localhost';
-const protocol = 'http';
-
-const baseURL = `${protocol}://${host}:${port}/api/v1`;
-
-export const api = axios.create({ baseURL });
