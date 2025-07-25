9b3ef57f426b0b99773400b7b9eed764fec1494e
satwikShresth <satwikshresth@gmail.com>
2025-07-25 18:30:47 -0400

    no more job listing

--- apps/front/src/components/nav/items.ts
+++ apps/front/src/components/nav/items.ts
@@ -1,10 +1,5 @@
 export const NAV_ITEMS = [
   {
-    label: "Job Listings",
-    href: "https://old.openmario.com/jobs",
-    section: "",
-  },
-  {
     label: "Courses",
     href: "/courses",
     section: "",
